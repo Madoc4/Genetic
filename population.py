d9273a645ac3014155b93a81066411da1bc9847f
Madoc <plowmanm@go.oes.edu>
2024-01-22 10:35:34 -0800

    added population by generation

--- population.py
+++ population.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 with open('species_history.txt', 'r') as f:
     lines = f.readlines()
 
-born = {}
-died = {}
+generations = {}
 
+for line in lines:
+    if line.startswith('Total generations: '):
+        g = int(line.split(' ')[2].strip())
+        for i in range(1, g+1):
+            generations[i] = 0
+    if line.startswith('Species with starting size'):
+        birth = int(line.split(' ')[10].replace('.',''))
+        death = int(line.split(' ')[12])
+        for i in range(birth, death+1):
+            generations[i] += 1
+
+plt.scatter(generations.keys(), generations.values(), color='blue')
+plt.xlabel('Generation')
+plt.ylabel('Number of Species')
+plt.title('Population by Generation')
+plt.show()
